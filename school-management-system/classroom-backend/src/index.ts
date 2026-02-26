e0f41e4b4c6fa937819702fcbf449ea8101235d8
George-Kibe <georgekibewambui@gmail.com>
2026-02-26 15:06:44 +0300

    arcject configuration

--- school-management-system/classroom-backend/src/index.ts
+++ school-management-system/classroom-backend/src/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import { uptime } from "node:process";
 import subjectsRouter from "./routes/subjects.js";
+import classesRouter from "./routes/classes.js";
+import usersRouter from "./routes/users.js";
+import departmentsRouter from "./routes/departments.js";
+import statsRouter from "./routes/stats.js";
+import enrollmentsRouter from "./routes/enrollments.js";
+
 import cors from "cors";
+import securityMiddleware from "./middlewares/security.js";
 
 const app = express();
 const PORT = 8000;
@@ -14,10 +21,16 @@ app.use(
   })
 );
 
-
-app.use("/api/subjects", subjectsRouter);
-
 app.use(express.json());
+app.use(securityMiddleware);
+
+// Routes
+app.use("/api/subjects", subjectsRouter);
+app.use("/api/classes", classesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/departments", departmentsRouter);
+app.use("/api/stats", statsRouter);
+app.use("/api/enrollments", enrollmentsRouter);
 
 app.get("/", (req, res) => {
   res.json({ 
